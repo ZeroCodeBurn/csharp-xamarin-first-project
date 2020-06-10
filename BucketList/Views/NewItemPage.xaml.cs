8cb12efef2b763a3b4945bdbff670772415c082f
Jim Correll <jim.correll@gmail.com>
2020-06-10 15:55:01 -0400

    Section 3

--- BucketList/Views/NewItemPage.xaml.cs
+++ BucketList/Views/NewItemPage.xaml.cs
@@ -1,43 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BucketList.Models;
+using System;
 using System.ComponentModel;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-using BucketList.Models;
 
 namespace BucketList.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
-    public partial class NewItemPage : ContentPage
+  // Learn more about making custom code visible in the Xamarin.Forms previewer
+  // by visiting https://aka.ms/xamarinforms-previewer
+  [DesignTimeVisible(false)]
+  public partial class NewItemPage : ContentPage
+  {
+    public Item Item { get; set; }
+
+    public NewItemPage()
     {
-        public Item Item { get; set; }
+      InitializeComponent();
 
-        public NewItemPage()
-        {
-            InitializeComponent();
+      Item = new Item
+      {
+        Text = "Item name",
+        Description = "This is an item description."
+      };
 
-            Item = new Item
-            {
-                Text = "Item name",
-                Description = "This is an item description."
-
-            };
-
-            BindingContext = this;
-        }
-
-        async void Save_Clicked(object sender, EventArgs e)
-        {
-            MessagingCenter.Send(this, "AddItem", Item);
-            await Navigation.PopModalAsync();
-        }
-
-        async void Cancel_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PopModalAsync();
-        }
+      BindingContext = this;
     }
+
+    private async void Save_Clicked(object sender, EventArgs e)
+    {
+      MessagingCenter.Send(this, "AddItem", Item);
+      await Navigation.PopModalAsync();
+    }
+
+    private async void Cancel_Clicked(object sender, EventArgs e)
+    {
+      await Navigation.PopModalAsync();
+    }
+  }
 }
