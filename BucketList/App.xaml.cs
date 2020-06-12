37d2033eea76058344573ee8fd7651c54af68cb4
Jim Correll <jim.correll@gmail.com>
2020-06-12 16:44:32 -0400

    Section 5

--- BucketList/App.xaml.cs
+++ BucketList/App.xaml.cs
@@ -1,32 +1,28 @@
-﻿using System;
+﻿using BucketList.Services;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-using BucketList.Services;
-using BucketList.Views;
 
 namespace BucketList
 {
-    public partial class App : Application
+  public partial class App : Application
+  {
+    public App()
     {
+      InitializeComponent();
 
-        public App()
-        {
-            InitializeComponent();
-
-            DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-        }
-
-        protected override void OnSleep()
-        {
-        }
-
-        protected override void OnResume()
-        {
-        }
+      DependencyService.Register<MockDataStore>();
+      MainPage = new AppShell();
     }
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnSleep()
+    {
+    }
+
+    protected override void OnResume()
+    {
+    }
+  }
 }
